f2fa80e1e07332982a805a4b66857adb7e4b074a
Cebisile16 <ngubsceby@gmail.com>
2026-05-20 05:59:50 +0000

    added the drone class

---
+++ src/main/java/Drone.java
@@ -0,0 +1,21 @@
+public class Drone {
+    private String id;
+    private int x;
+    private int y;
+    private int battery;
+    private String direction;
+
+
+    public drone(String id, int x, int y, int battery, String direction){
+        this.id =id;
+        this.x = x;
+        this.y = y;
+        this.battery = battery;
+        this.direction = direction; 
+
+    }
+
+    
+
+
+}
